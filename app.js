732981433fc5a2c003743d4d566d9fcac5e45ee6
carloslee008 <59149514+carloslee008@users.noreply.github.com>
2022-11-10 01:39:05 -0500

    update to hashing passwords

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 // const bodyParser = require("body-parser");
 
@@ -27,8 +27,6 @@ async function main() {
     password: String
   });
 
-  userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
   const User = mongoose.model('User', userSchema);
 
   app.get("/", function(req, res){
@@ -41,7 +39,7 @@ async function main() {
     })
     .post((req, res) => {
       const username = req.body.username;
-      const password = req.body.password;
+      const password = md5(req.body.password);
 
       User.findOne({email: username}, function(err, user){
         if (user){
@@ -62,7 +60,7 @@ async function main() {
     .post((req, res) => {
       const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
       });
 
       newUser.save(function(err){
